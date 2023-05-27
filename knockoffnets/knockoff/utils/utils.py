8dae9a0cc8718c7fcea19a0d11e465c556cbc4c0
KostyaPoberezhnyuk <96181906+DoctorVooDoo@users.noreply.github.com>
2023-05-27 23:32:27 +0300

    Delete knockoffnets directory

--- knockoffnets/knockoff/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path as osp
-
-
-def create_dir(dir_path):
-    if not osp.exists(dir_path):
-        print('Path {} does not exist. Creating it...'.format(dir_path))
-        os.makedirs(dir_path)
